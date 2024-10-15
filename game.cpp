8c15e2681a6d3927d37f22d9f4ca9782a90be3c8
Safee Ahmed <safee.bangash@gmail.com>
2024-10-15 18:39:58 +0500

    Radomized Object Position, Onbject Placing and Player Movement basic

--- game.cpp
+++ game.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<cstdlib>
 using namespace std;
 
 class Node {
@@ -14,13 +15,23 @@ class Board {
 public:
     Node* head;
     int size;
+    int px;
+    int py;
+    int kx;
+    int ky;
+    int dx;
+    int dy;
+    int c1x,c1y;
+    int c2x,c2y;
+    Node*player;
+
 
     Board(int level) {
         if (level == 1) {
             size = 10;
         }
         else if(level==2){
-            size = 16;
+            size = 15;
         }
         else if(level==3){
             size = 20;
@@ -85,6 +96,104 @@ public:
         head = headArr[0];
     }
 
+
+    void setParameters(){
+        px=rand()%(size-2);
+        py=rand()%(size-2);
+        
+        while(true){
+            kx=rand()%(size-2);
+            ky=rand()%(size-2);
+            if(kx!=px && ky!=py){
+                break;
+            }
+        }
+        while(true){
+            dx=rand()%(size-2);
+            dy=rand()%(size-2);
+            if(dx!=px && dy!=py && dx!=kx && dy!=ky){
+                break;
+            }
+        }
+        while(true){
+            c1x=rand()%(size-2);
+            c1y=rand()%(size-2);
+            if(c1x!=px && c1y!=py && c1x!=kx && c1y!=ky && c1x!=dx &&c1y!=dy){
+                break;
+            }
+        }
+        while(true){
+            c2x=1+rand()%(size-2);
+            c2y=1+rand()%(size-2);
+            if(c2x!=px && c2y!=py && c2x!=kx && c2y!=ky && c2x!=dx &&c2y!=dy &&c2x!=c1x &&c2y!=c1y){
+                break;
+            }
+        }
+        cout<<px<<" "<<py<<" "<<dx<<" "<<dy<<" "<<kx<<" "<<ky<<" "<<c1x<<" "<<c1y<<endl;
+    }
+    void setPositions(){
+        Node*currentRow=head->down;
+        for (int a=0;a<size-2;a++){
+            Node*currentPtr=currentRow->right;
+            for(int b=0;b<size-2;b++){
+                if(b==px && a==py){
+                    currentPtr->key='P';
+                    player=currentPtr;
+                }
+                else if(b==kx && a==ky){
+                    currentPtr->key='K';
+                }
+                else if(b==dx && a==dy){
+                    currentPtr->key='D';
+                }
+                else if(b==c1x && a==c1y){
+                    currentPtr->key='C';
+                }
+                else if(b==c2x && a==c2y){
+                    currentPtr->key='C';
+                }
+                currentPtr=currentPtr->right;
+            }
+        currentRow=currentRow->down;
+        }
+    }
+    void MovePlayer(){
+        char dir;
+        cout<<"Give Input";
+        cin>>dir;
+        if(dir=='w'){
+            if(py>0){
+                py--;
+                player->key='.';
+                player=player->up;
+                player->key='P';
+            }
+        }
+        else if(dir=='s'){
+            if(py<size-2){
+                py++;
+                player->key='.';
+                player=player->down;
+                player->key='P';
+            }
+        }
+        else if(dir=='d'){
+            if(px<size-2){
+                px++;
+                player->key='.';
+                player=player->right;
+                player->key='P';
+        }
+        }
+        else if(dir=='a'){
+            if(px>0){
+                px--;
+                player->key='.';
+                player=player->left;
+                player->key='P';
+            }
+        }
+    }
     void printBoard(){
             Node* currentRow = head;
 
@@ -101,10 +210,20 @@ public:
     }
 };
 
-int main() {
+void PlayGame(){
     Board b1(3);
+    b1.setParameters();
+    b1.setPositions();
     b1.printBoard();
-
+    while(true){
+        b1.MovePlayer();
+        b1.printBoard();
+    }
+}
+int main() {
+    
+    srand(time(0));
+    PlayGame();
 
     return 0;
 }
