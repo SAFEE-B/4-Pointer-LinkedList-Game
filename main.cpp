84dd4454d0c4c0a4e51d1c5fc41f646d8b1a2276
Safee Ahmed <safee.bangash@gmail.com>
2024-10-20 19:38:46 +0500

    Made final Modifications. Added features.Corrected Errors

--- main.cpp
+++ main.cpp
@@ -4,7 +4,63 @@
 #include <curses.h> // Ensure you have included the PDCurses header
 #include<math.h>
 #include<string>
+
 using namespace std;
+
+
+
+class NodeQ{
+    public:
+    int x;
+    int y;
+    NodeQ*  next;
+};
+class Queue {
+    NodeQ* head;
+
+public:
+    Queue() {
+        head = nullptr;
+    }
+
+    void push(int x, int y) {
+        NodeQ* newNode = new NodeQ();
+        newNode->x = x;
+        newNode->y = y;
+        newNode->next = nullptr;
+
+        // If the queue is empty, set the new node as the head.
+        if (!head) {
+            head = newNode;
+        } else {
+            NodeQ* currentPtr = head;
+            while (currentPtr->next) {
+                currentPtr = currentPtr->next;
+            }
+            currentPtr->next = newNode;
+        }
+    }
+
+    void PrintAll() {
+    NodeQ* currentPtr = head;
+    while (currentPtr) {
+        printw("Coin -( %d , %d )   ", currentPtr->x, currentPtr->y); // Print the formatted string
+        currentPtr = currentPtr->next;
+    }
+    printw("\n"); 
+}
+
+    // Destructor to clean up memory if necessary
+    ~Queue() {
+        NodeQ* currentPtr = head;
+        while (currentPtr->next) {
+            NodeQ* nextPtr = currentPtr->next;
+            delete currentPtr;
+            currentPtr = nextPtr;
+        }
+    }
+};
+
 
 class Node {
 public:
@@ -14,6 +70,8 @@ public:
     Node* left;
     Node* right;
 };
+
+
 
 class Board {
 public:
@@ -27,27 +85,46 @@ public:
     int ky;
     int dx;
     int dy;
+    int bx,by;
     int c1x,c1y;
     int c2x,c2y;
     Node*player;
     bool closerOrFarther;
     bool keyStatus;
-
+    int undoCount;
+    char preDir;
+    int score;
+    int coinCollected;
+    int moveCount;
+    Queue coinsQueue;
+    bool gameOver,gameWin;
     Board(int level) {
+        gameOver=false;
+        gameWin=false;
+        coinCollected=0;
         keyStatus=false;
         if (level == 1) {
             size = 10;
+            undoCount=6;
+            moveCount=6;
         }
         else if(level==2){
             size = 15;
+            undoCount=4;
+            moveCount=2;
         }
         else if(level==3){
             size = 20;
+            undoCount=1;
+            moveCount=1;
         }
         else{
             size = 10;
+            undoCount=6;
+            moveCount=6;
             printw("Invalid Number Given as Size...(Started with size 10x10)");
         }
+        score=undoCount;
         Node* headArr[size];
         for (int a = 0; a < size; a++) {
             for(int b=0;b<size;b++){
@@ -104,7 +181,12 @@ public:
         head = headArr[0];
     }
 
-
+    bool getGameOver(){
+        return gameOver;
+    }
+    bool getGameWin(){
+        return gameWin;
+    }
     void setParameters(){
         px=rand()%(size-2);
         py=rand()%(size-2);
@@ -137,7 +219,17 @@ public:
                 break;
             }
         }
+        while (true) {
+            bx = rand() % (size - 2);
+            by = rand() % (size - 2);
+            if (bx != px && by != py && bx != kx && by != ky && bx != dx && by != dy && bx != c1x && by != c1y && bx != c2x && by != c2y) {
+                break;
+            }
+        }
+
+        moveCount+=getTotalDistance();
     }
+    
     void setPositions(){
         Node*currentRow=head->down;
         for (int a=0;a<size-2;a++){
@@ -148,10 +240,10 @@ public:
                     player=currentPtr;
                 }
                 else if(b==kx && a==ky){
-                    currentPtr->key='K';
+                    currentPtr->key='.';
                 }
                 else if(b==dx && a==dy){
-                    currentPtr->key='D';
+                    currentPtr->key='.';
                 }
                 else if(b==c1x && a==c1y){
                     currentPtr->key='C';
@@ -159,11 +251,15 @@ public:
                 else if(b==c2x && a==c2y){
                     currentPtr->key='C';
                 }
+                else if(b==bx&&a==by){
+                    currentPtr->key='B';
+                }
                 currentPtr=currentPtr->right;
             }
         currentRow=currentRow->down;
         }
     }
+
     void setVal(int x,int y,char val){
         Node*currentRow=head->down;
         bool bool1=false;
@@ -183,6 +279,8 @@ public:
         currentRow=currentRow->down;
     }
     }
+
+
     void randomizeCoin(){
         setVal(c1x,c1y,'.');
         setVal(c2x,c2y,'.');
@@ -206,6 +304,21 @@ public:
     bool checkKeyCoincide(){
         return (kx==px && ky==py);
     }
+    bool checkCoinCoincide(){
+        if(c1x==px&&c1y==py){
+            coinsQueue.push(c1x,c1y);
+            undoCount++;
+            return true;
+        }
+        else if(c2x==px&&c2y==py){
+            coinsQueue.push(c2x,c2y);
+            undoCount++;
+            return true;
+
+        }
+        return false;
+    }
+
     void MovePlayer(){
         int dist_i=getDistance();
         char dir;
@@ -213,88 +326,177 @@ public:
         cin>>dir;
         if(dir=='w'){
             if(py>0){
-                py--;
-                player->key='.';
-                player=player->up;
-                player->key='P';
+                if(preDir=='s' && undoCount==0){
+                }
+                else{
+                if(preDir=='s'){
+                    undoCount--;
+                }
+                else{
+                    moveCount--;
+                }
+                    py--;
+                    player->key='.';
+                    player=player->up;
+                    player->key='P';
+                    preDir=dir;
+                }
             }
         }
         else if(dir=='s'){
             if(py<size-3){
-                py++;
-                player->key='.';
-                player=player->down;
-                player->key='P';
+                if(preDir=='w' && undoCount==0){
+                }
+                else{
+                if(preDir=='w'){
+                    undoCount--;
+                }
+                else{
+                    moveCount--;
+                }
+                    py++;
+                    player->key='.';
+                    player=player->down;
+                    player->key='P';
+                    preDir=dir;
+                }
             }
         }
         else if(dir=='d'){
             if(px<size-3){
-                px++;
-                player->key='.';
-                player=player->right;
-                player->key='P';
-        }
+                if(preDir=='a' && undoCount==0){
+                }
+                else{
+                if(preDir=='a'){
+                    undoCount--;
+                }
+                else{
+                    moveCount--;
+                }
+                    px++;
+                    player->key='.';
+                    player=player->right;
+                    player->key='P';
+                    preDir=dir;
+                }
+            }
         }
         else if(dir=='a'){
             if(px>0){
-                px--;
-                player->key='.';
-                player=player->left;
-                player->key='P';
-            }
+                if(px>0){
+                    if(preDir=='d' && undoCount==0){
+                    }
+                    else{
+                    if(preDir=='d'){
+                        undoCount--;
+                    }
+                    else{
+                        moveCount--;
+                    }
+                        px--;
+                        player->key='.';
+                        player=player->left;
+                        player->key='P';
+                        preDir=dir;
+                    }
+                }
+        }
+        }
+        int dist_f=getDistance();
+        if(dist_f>dist_i){
+            closerOrFarther=true;
+        }
+        else{
+            closerOrFarther=false;
         }
         if(!keyStatus){
-            int dist_f=getDistance();
-            if(dist_f>dist_i){
-                closerOrFarther=true;
-            }
-            else{
-                closerOrFarther=false;
-            }
             keyStatus=checkKeyCoincide();
-            }
-        
+        }
+        if(checkCoinCoincide()){
+            coinCollected++;
+        }
+        score=undoCount+(coinCollected*2);
+        gameWin=(px == dx && py == dy && keyStatus);
+        gameOver= (moveCount <= 0)||(bx==px && by==py);
     }
+    
+    
     int getDistance(){
-        return abs(kx-px)+abs(ky-py);
+        if(!keyStatus)
+            return abs(kx-px)+abs(ky-py);
+        else{
+            return abs(dx-px)+abs(dy-py);
+        }
     }
+    int getTotalDistance(){
+        return abs(kx-px)+abs(ky-py)+abs(kx-dx)+abs(ky-dy);
+    }
+
     void printBoard() {
         clear(); // Clear the screen
         Node* currentRow = head;
-        mvprintw(2,5,"SAFEE");
+
+        if(gameOver||gameWin){
+        setVal(kx, ky, 'K'); // Make the key visible
+        setVal(dx, dy, 'D'); // Make the door visible
+        displayCollectedCoins();
+        }
+        string Score="Score: "+to_string(score);
+        mvprintw(1,2,Score.c_str());
+        string MvCnt="Moves Left: "+to_string(moveCount);
+        mvprintw(2,2,MvCnt.c_str());
+        string UndoCnt="Undo Count: "+to_string(undoCount);
+        mvprintw(3,2,UndoCnt.c_str());
         string str;
         if(!keyStatus){
-        if(!closerOrFarther ){
-            str="Distance to Key: Getting Closer";
+            if(!closerOrFarther ){
+                str="Distance to Key: Getting Closer";
+            }
+            else{
+                str="Distance to Key: Getting Farther";
+            }
         }
         else{
-            str="Distance to Key: Getting Farther";
+            if(!closerOrFarther ){
+            str="Distance to Door: Getting Closer";
+            }
+            else{
+                str="Distance to Door: Getting Farther";
+            }
         }
-        mvprintw(2,10,str.c_str());
-        }
-        else{
+        mvprintw(4,2,str.c_str());
+        
+        if(keyStatus){
             str="Key Collected";
-            mvprintw(2,10,str.c_str());
+            mvprintw(5,2,str.c_str());
         }
+        
         // Print the board row by row
         for (int a = 0; a < size; a++) {
             Node* current = currentRow;
             for (int b = 0; b < size; b++) {
-                mvaddch(5+a*2,5+b*4, current->key); // Use PDCurses to add characters
+                mvaddch(6+a*2,10+b*4, current->key); 
                 current = current->right;
             }
             currentRow = currentRow->down;
         }
-        refresh(); // Refresh the screen to show changes
+        refresh(); 
     }
+    void displayCollectedCoins(){
+        coinsQueue.PrintAll();
+    }
+    void printGameOverState() {
+    printBoard();
+    printw("\nPress any key to exit...");
+    refresh();
+    getch(); // Wait for the user to press any key before exiting
+}
 };
 
 void PlayGame(){
-    
     printw("What Hardness You wanna Play...\nPRESS 1 :EASY\nPRESS 2:MEDIUM\nPRESS 3:HARD");
     int hardness;
     scanw("%d",&hardness);
-    flash();
     Board b1(hardness);
     b1.setParameters();
     b1.setPositions();
@@ -307,6 +509,15 @@ void PlayGame(){
         if(count%10==0){
             b1.randomizeCoin();
         }
+        if (b1.getGameWin()) {
+            b1.printGameOverState();
+            printw("Congratulations! You reached the door and won the game.\n");
+            break;
+        } else if (b1.getGameOver()) {
+            b1.printGameOverState();
+            printw("Game Over! \n");
+            break;
+        }
     }
 }
 int main() {
@@ -315,6 +526,5 @@ int main() {
     noecho(); // Don't echo user input
     //cbreak(); // Disable line buffering
     PlayGame();
-    endwin(); // End PDCurses mode
     return 0;
 }
